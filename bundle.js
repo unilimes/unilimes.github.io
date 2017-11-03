17f296564796e4a33915cf12021eb2a4cd322659
unilimes <unilimesdevs@gmail.com>
2017-11-03 18:01:50 +0200

    first commit block total

--- bundle.js
+++ bundle.js
@@ -45673,10 +45673,10 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log(document.getElementById('Armrest').value);
             console.log(document.getElementById('Backrest').value);
             console.log(document.getElementById('Cushion').value);
-            setTimeout(function () { document.getElementById('yui_3_17_2_3_1509719000525_370').click(); }, 1000);
-            setTimeout(function () { document.getElementById('yui_3_17_2_4_1509719314638_370').click(); }, 1500);
-            setTimeout(function () { document.getElementById('yui_3_17_2_4_1509719218279_369').click(); }, 2000);
-            setTimeout(function () { document.getElementById('yui_3_17_2_6_1509719314638_370').click(); }, 2500);
+            setTimeout(function () { document.getElementById('yui_3_17_2_3_1509719000525_370').click(); }, 100);
+            setTimeout(function () { document.getElementById('yui_3_17_2_4_1509719314638_370').click(); }, 500);
+            setTimeout(function () { document.getElementById('yui_3_17_2_4_1509719218279_369').click(); }, 1000);
+            setTimeout(function () { document.getElementById('yui_3_17_2_6_1509719314638_370').click(); }, 1500);
             // const anchor = document.createElement('a');
             // anchor.setAttribute('href','/cart');
             // document.body.appendChild(anchor);
