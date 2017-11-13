8aed96f22e7205d8daf1e4c69c5d5f76d1d152a7
unilimes <unilimesdevs@gmail.com>
2017-11-13 15:39:38 +0200

    test

--- src/floor.ts
+++ src/floor.ts
@@ -20,7 +20,7 @@ export class Floor{
         /* add mirrored surface */
         this.bufferedGeometry = new THREE.PlaneBufferGeometry(1024,1024)
         this.geometry = new THREE.PlaneGeometry(1024,1024)
-        this.groundMirror = Mirror()(
+        this.groundMirror = Mirror(THREE)(
             renderer,
             camera, {clipBias : 0.003,textureWidth: 1024, textureHeight: 1024,color:MIRROR_COLOR})
         this.material = new THREE.MeshPhongMaterial({
