98fc893eaa7ad515dcd482ee921f975152fe17c9
unilimes <unilimesdevs@gmail.com>
2017-11-15 12:59:00 +0200

    test

--- bundle.js
+++ bundle.js
@@ -45695,10 +45695,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById(id).style.display = 'none';
                 console.log(document.querySelectorAll(data)[0].children[0].value);
             }
-            setTimeout(addToCart, 500, 'yui_3_17_2_1_1509985394439_153', "[data-item-id='59fc7b18ec212dd8d9a3661a'", window['kopa_tally'].sofa);
-            setTimeout(addToCart, 700, 'yui_3_17_2_1_1509984998759_152', "[data-item-id='59fc7b58652deac953923e02'", window['kopa_tally'].back);
-            setTimeout(addToCart, 900, 'yui_3_17_2_1_1509984706599_152', "[data-item-id='59f716b424a694055a7c20c0'", window['kopa_tally'].arm);
-            setTimeout(addToCart, 1100, 'yui_3_17_2_1_1509985286676_152', "[data-item-id='59fc7b5f24a694eecd2344c2'", window['kopa_tally'].cushion);
+            setTimeout(addToCart, 1000, 'yui_3_17_2_1_1509985286676_152', "[data-item-id='59fc7b5f24a694eecd2344c2'", window['kopa_tally'].cushion);
+            setTimeout(addToCart, 1000, 'yui_3_17_2_1_1509984706599_152', "[data-item-id='59f716b424a694055a7c20c0'", window['kopa_tally'].arm);
+            setTimeout(addToCart, 1000, 'yui_3_17_2_1_1509984998759_152', "[data-item-id='59fc7b58652deac953923e02'", window['kopa_tally'].back);
+            setTimeout(addToCart, 1000, 'yui_3_17_2_1_1509985394439_153', "[data-item-id='59fc7b18ec212dd8d9a3661a'", window['kopa_tally'].sofa);
             // const anchor = document.createElement('a');
             // anchor.setAttribute('href','/cart');
             // document.body.appendChild(anchor);
