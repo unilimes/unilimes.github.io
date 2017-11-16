ca93523445de52dd287f9790880fb8b0e978672f
unilimes <unilimesdevs@gmail.com>
2017-11-16 16:27:08 +0200

    test

--- bundle.js
+++ bundle.js
@@ -45695,14 +45695,26 @@ document.addEventListener('DOMContentLoaded', function () {
             if (window['kopa_tally'].cushion > 0) {
                 setTimeout(addToCart, 500, 'yui_3_17_2_1_1509985286676_152', "[data-item-id='59fc7b5f24a694eecd2344c2'", window['kopa_tally'].cushion);
             }
+            else if (window['kopa_tally'].cushion > 1) {
+                setTimeout(addToCart, 300, 'yui_3_17_2_1_1509985286676_152', "[data-item-id='59fc7b5f24a694eecd2344c2'", window['kopa_tally'].cushion);
+            }
             if (window['kopa_tally'].arm > 0) {
                 setTimeout(addToCart, 900, 'yui_3_17_2_1_1509984998759_152', "[data-item-id='59fc7b58652deac953923e02'", window['kopa_tally'].arm);
+            }
+            else if (window['kopa_tally'].arm > 1) {
+                setTimeout(addToCart, 500, 'yui_3_17_2_1_1509984998759_152', "[data-item-id='59fc7b58652deac953923e02'", window['kopa_tally'].arm);
             }
             if (window['kopa_tally'].back > 0) {
                 setTimeout(addToCart, 1300, 'yui_3_17_2_1_1509985394439_153', "[data-item-id='59fc7b18ec212dd8d9a3661a'", window['kopa_tally'].back);
             }
+            else if (window['kopa_tally'].back > 1) {
+                setTimeout(addToCart, 700, 'yui_3_17_2_1_1509985394439_153', "[data-item-id='59fc7b18ec212dd8d9a3661a'", window['kopa_tally'].back);
+            }
             if (window['kopa_tally'].sofa > 0) {
                 setTimeout(addToCart, 1700, 'yui_3_17_2_1_1509984706599_152', "[data-item-id='59f716b424a694055a7c20c0'", window['kopa_tally'].sofa);
+            }
+            else if (window['kopa_tally'].sofa > 1) {
+                setTimeout(addToCart, 1000, 'yui_3_17_2_1_1509984706599_152', "[data-item-id='59f716b424a694055a7c20c0'", window['kopa_tally'].sofa);
             }
             // function linkToCart(){
             //     const anchor = document.createElement('a');
